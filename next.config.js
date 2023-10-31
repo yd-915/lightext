cd2d46a1f84ddc65796cf9616210590dc3218b7d
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-10-31 19:22:18 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,8 @@ const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS(
-  
-    withSaas({
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-    })
+
 );
