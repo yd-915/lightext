75bd1fca95edb4c5cc148f783fe94101cb250606
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-10-31 13:03:17 -0400

    Update env-config.js

--- env-config.js
+++ env-config.js
@@ -2,7 +2,7 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.BASE_URL': prod ? 'https://lightext-production.up.railway.app/',
+  'process.env.BASE_URL': 'https://lightext-production.up.railway.app/',
   'process.env.NAMESPACE': 'https://lightext-production.up.railway.app/',
   'process.env.CLIENT_ID': 'VHnZ3TrfHG2tBKbWdslI8x2PHlPyzTfX'
 }
